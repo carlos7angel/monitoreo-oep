25ca7ca1b3ca7cebe916604d2ac159d8ad0430f6
Carlos Alejo <carlos7angel@gmail.com>
2024-09-29 12:49:09 -0400

    views

--- storage/framework/views/c11b9486073ffd2682b4216ac65af4ca.php
+++
@@ -1,4 +0,0 @@
-<div id="kt_scrolltop" class="scrolltop" data-kt-scrolltop="true">
-    <i class="ki-outline ki-arrow-up"></i>
-</div>
-<?php /**PATH D:\Development Environment\PHP Environment\Laragon\www\monitoreo-oep\app\Containers\Vendor\Template/UI/WEB/Views//landing/components/partials/_scrolltop.blade.php ENDPATH**/ ?>
--- storage/framework/views/f1d9f4b6caea8af4dee2cb110dcd07b1.php
+++
@@ -1,5 +0,0 @@
-<!--begin::Scrolltop-->
-<div id="kt_scrolltop" class="scrolltop" data-kt-scrolltop="true">
-    <i class="ki-outline ki-arrow-up"></i>
-</div>
-<!--end::Scrolltop--><?php /**PATH D:\Development Environment\PHP Environment\Laragon\www\monitoreo-oep\app\Containers\Vendor\Template/UI/WEB/Views//external/components/partials/_scrolltop.blade.php ENDPATH**/ ?>
