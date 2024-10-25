32bf22be4683ad86a771a8c3f89f4a5f23b86df7
Carlos Alejo <carlos7angel@gmail.com>
2024-10-24 23:10:50 -0400

    add sql file as seeder

---
+++ app/Containers/CoreMonitoring/FormBuilder/Data/Seeders/FormFieldsSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Containers\CoreMonitoring\FormBuilder\Data\Seeders;
+
+use App\Ship\Parents\Seeders\Seeder as ParentSeeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\File;
+
+class FormFieldsSeeder extends ParentSeeder
+{
+    public function run(): void
+    {
+        Model::unguard();
+
+        $filePath = database_path('fieldTypes.sql');
+
+        if(File::exists($filePath)){
+            DB::unprepared(file_get_contents($filePath));
+            $this->command->info('Field Types Table Seed');
+        }
+
+    }
+}
